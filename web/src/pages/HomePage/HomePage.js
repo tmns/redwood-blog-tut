ebde4cda1ba863d07946ab98d2ac2c8cd6dc8b41
methias <tmn5@pm.me>
2020-03-14 21:07:31 +0200

    add home route

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>Welcome to my new & (not really) shiny RedwoodJS powered blog!</p>
+    </div>
+  )
+}
+
+export default HomePage
