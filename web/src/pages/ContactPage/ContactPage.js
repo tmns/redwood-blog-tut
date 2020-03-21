aae0b2568096fd300c08108ecf3c76ca18cb7fd6
methias <tmn5@pm.me>
2020-03-21 13:33:37 +0200

    build out contact form

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,7 +1,76 @@
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+  Label,
+} from '@redwoodjs/web'
 import BlogLayout from 'src/layouts/BlogLayout'
 
 const ContactPage = () => {
-  return <BlogLayout></BlogLayout>
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <BlogLayout>
+      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+        <Label
+          htmlFor="name"
+          style={{ display: 'block' }}
+          errorStyle={{ display: 'block', color: 'red' }}
+        >
+          Name
+        </Label>
+        <TextField
+          name="name"
+          style={{ display: 'block' }}
+          errorStyle={{ display: 'block', borderColor: 'red' }}
+          validation={{ required: true }}
+        />
+        <FieldError name="name" style={{ color: 'red' }} />
+
+        <Label
+          htmlFor="email"
+          style={{ display: 'block' }}
+          errorStyle={{ display: 'block', color: 'red' }}
+        >
+          Email
+        </Label>
+        <TextField
+          name="email"
+          style={{ display: 'block' }}
+          errorStyle={{ display: 'block', borderColor: 'red' }}
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^\.]+\..+/,
+              message: 'Please enter a valid email address.',
+            },
+          }}
+        />
+        <FieldError name="email" style={{ color: 'red' }} />
+
+        <Label
+          htmlFor="message"
+          style={{ display: 'block' }}
+          errorStyle={{ display: 'block' }}
+        >
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          style={{ display: 'block' }}
+          errorStyle={{ display: 'block', borderColor: 'red' }}
+          valdation={{ required: true }}
+        />
+        <FieldError name="message" style={{ color: 'red' }} />
+
+        <Submit style={{ display: 'block' }}>Save</Submit>
+      </Form>
+    </BlogLayout>
+  )
 }
 
 export default ContactPage
